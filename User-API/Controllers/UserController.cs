d5c531833bfe600527666d8bf716a6ba6ae8ec87
Mariano <marianozamorach@gmail.com>
2023-03-07 22:29:19 -0500

    add test: crud users

---
+++ User-API/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
+
+namespace UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetSuperHeroes()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> CreateSuperHero(User hero)
+        {
+            _context.Users.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Users>>> UpdateSuperHero(User hero)
+        {
+            var dbUser = await _context.Users.FindAsync(hero.Id);
+            if (dbUser == null)
+                return BadRequest("Hero not found.");
+
+            dbUser.Name = hero.Name;
+            dbUser.FirstName = hero.FirstName;
+            dbUser.LastName = hero.LastName;
+            dbUser.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Users>>> DeleteSuperHero(int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+            if (dbUser == null)
+                return BadRequest("Hero not found.");
+
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+    }
+}
